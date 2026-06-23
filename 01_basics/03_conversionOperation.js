d593ef16fd92137290471e8fb88be4abbfc103ee
datralele-cyber <datralele@gmail.com>
2026-06-23 08:53:57 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = true 
+
+
+// const {score} = req.body
+// console.log(typeof score);
+// console.log(typeof (score));
+ 
+
+let valueInNUmber = Number (score);
+// console.log ( typeof valueInNUmber);
+// console.log ( valueInNUmber);
+
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ;false =>0
+
+
+let isLoggedIn = 1 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1=> true; flase=>0
+//  "" => false 
+// "hitesh " => true 
+
+
+
+let  someNumber  = 33 
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
