bbfb20d9f656d492b4f8e2e3201f3cb00a675aa1
datralele-cyber <datralele@gmail.com>
2026-06-24 10:34:29 +0000

    conversion and operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,7 +21,7 @@ let valueInNUmber = Number (score);
 let isLoggedIn = 1 
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1=> true; flase=>0
 //  "" => false 
@@ -32,5 +32,62 @@ console.log(booleanIsLoggedIn);
 let  someNumber  = 33 
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+
+
+
+// ******************   operations   *************************
+
+
+let value = 3
+
+let negValue = -value 
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello "
+let str2 = "Ishmeet "
+
+
+let str3 =  str1+str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2" );
+// console.log("1" + 2 + 2);
+// console.log(1+ 2 + "2");
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1 ,num2, num3 ;
+num1 = num2 = num3 = 2+2;
+
+
+ let gameCounter = 100
+
+++gameCounter;
+
+gameCounter++;
+
+console.log(gameCounter);
+
+
+
+
+
