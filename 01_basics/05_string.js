b83f09151898f74862715ea5c1a50153e7939ee4
datralele-cyber <datralele@gmail.com>
2026-07-06 18:12:49 +0000

    strings in depth

---
+++ 01_basics/05_string.js
@@ -0,0 +1,44 @@
+const name = "IShmeet Singh Sodhi "
+const repoCount = 50 
+
+// console.log(name + repoCount + "   Value");
+
+console.log(`Hello my name is ${name} and my repo cpont is ${
+    repoCount
+}`);
+
+const gameName = new String('Ishmeet_Singh_sodhi')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(6));
+// console.log(gameName.indexOf("t"));
+
+
+
+// const NewStringName = gameName.substring(0,4);   // in the sub string format we cannot give the negative value 
+// console.log(NewStringName);
+
+// const anotherString = gameName.slice(-5,0); // in the slice we can give the negative value the last is considere as the -1 as I am goung through 
+
+// console.log(anotherString);
+
+
+
+const newString01 = "         Ishmeet         ";
+console.log(newString01);
+console.log(newString01.trim());
+
+
+const url = "https//IshmeetSInghSOdhi.com/20%Ishmeet"
+
+console.log(url.replace('20%', '__'));
+console.log(url.includes("Sundar"))
+
+console.log(gameName.split('_'));
+
+
