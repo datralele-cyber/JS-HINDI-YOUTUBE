c653af03c1857efc2ca17c341caf6919bd375fd5
datralele-cyber <datralele@gmail.com>
2026-07-06 12:37:39 +0000

    datatypes-summery addition

---
+++ 01_basics/datatypes-summery.js
@@ -0,0 +1,41 @@
+//  # Primitive Data Type or referenced 
+
+
+//   7 types : String , Number , Boolean , null , undefined ,Symbol , BigInt 
+
+
+const value = 100
+const  scoreValue = 100.3
+
+const isLoggedIn = false 
+
+const outSideTemp = null
+const userEmail = undefined
+// const  userEmail;
+
+const Id = Symbol("123")
+const anotherID = Symbol("123")
+
+console.log( Id === anotherID)
+
+const bigNumber = 12023321321322311n 
+
+
+const hereos = [shaiktiman , nagaraja , dodabarfi ];
+let myObj = {
+    name = "Ishmeet Singh Sodhi ",
+    age = 22,
+}
+
+const myFunction = function (){
+
+    console.log("Hello World ");
+}
+
+console.log(typeof hereos)
+console.log(typeof bigNumber);
+
+
+// Referance or (Non.  premative) 
+
+//  Array , Objects , Functions 
