5b0673a47aba53b7107904cd91adc0f6147cb0fa
datralele-cyber <datralele@gmail.com>
2026-07-12 20:34:21 +0000

    aaj ka ho liya re

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,30 @@
+// DATES 
+
+
+
+
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate );
+
+
+
+
+
+// let myCreatedDate = new Date (2023 , 0 , 23)
+// let myCreatedDate = new Date ( "2023-01-14")
+let myCreatedDate = new Date ( "2023-01-14")
+// console.log(myCreatedDate.toDateString());
+
+
+
+let myTimeStamp = Data.now()
+// console.log(myTeamStamp);
+console.log(myCreateDated.getTime());
+console.log(Date.now()/1000);
+
+
+
