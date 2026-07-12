97b5d4d14aa5c4d7617e714913bda5b84ca739b2
datralele-cyber <datralele@gmail.com>
2026-07-12 20:01:45 +0000

    updated num_maths faaaakaakaka

--- 01_basics/06_num_and_maths.js
+++ 01_basics/06_num_and_maths.js
@@ -1,9 +1,51 @@
-const score = 400 
-console.log(score)
+// const score = 400 
+// console.log(score)
 
 
 
 
-const balance = new Number(100);
-console.log(balance.toString().length);
-console.log(balance.tofixed(2));
+// const balance = new Number(100);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+// const  otherNumber = 123.23555425
+// console.log(otherNumber.toPrecision(4));
+
+
+// const hundreds = 1000000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.9));
+// console.log(Math.ceil(4.2));
+// console.log(Math.min(4,2,6,9,10));
+// console.log(Math.max(4,5,6,8,9));
+
+
+
+// Math.random is an important Discussion why it is used and what it is used for 
+console.log(Math.random());
+console.log((Math.random()*10) + 1 );
+console.log(Math.floor(Math.random()*10) + 1 );
+
+
+const min = 10
+const max = 20
+
+
+
+console.log(Math.random() *(max - min +1  ) + min )
+
+
