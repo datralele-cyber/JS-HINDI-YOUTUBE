1c7d616bbd936a9485ed2fc302f4dcc908274a74
datralele-cyber <datralele@gmail.com>
2026-07-17 07:41:36 +0000

    array learning in is array , flat and from my more will be moving to the objects in it thanks ...

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,121 @@
+const heros_marval = ["spiderman ", "ironman ","loki "];
+const heros_dc  = ["batman " ,"Superman " ,"Wonder women "];
+
+
+// heros_dc.push(heros_dc);
+// console.log(heros_marval)
+// console.log(heros_dc);
+// console.log(heros_dc[3][1]);
+
+
+// const allheroes = heros_dc.concat(heros_marval)
+// // console.log(heros_marval);
+// // console.log(heros_dc);
+// console.log(allheroes);
+
+const all_new_heros = [...heros_dc, ...heros_marval];
+// console.log(all_new_heros);
+ 
+
+const another_arr = [ 1,2,3,[4,5,6],7,[8,9,10,11,[12,1,4,]]]  
+
+const real_another_array = another_arr.flat(Infinity);
+
+console.log(real_another_array);
+
+
+
+
+
+
+console.log(Array.isArray("Ishmeet Singh Sodhi "))
+console.log(Array.from("Ishmeet Singh Sodhi "))
+console.log(Array.from({name: "Hitesh"})) //// instresting case asked in the interview
+
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of (score1,score2,score3));   const heros_marval = ["spiderman ", "ironman ","loki "];
+const heros_dc  = ["batman " ,"Superman " ,"Wonder women "];
+
+
+// heros_dc.push(heros_dc);
+// console.log(heros_marval)
+// console.log(heros_dc);
+// console.log(heros_dc[3][1]);
+
+
+// const allheroes = heros_dc.concat(heros_marval)
+// // console.log(heros_marval);
+// // console.log(heros_dc);
+// console.log(allheroes);
+
+const all_new_heros = [...heros_dc, ...heros_marval];
+// console.log(all_new_heros);
+ 
+
+const another_arr = [ 1,2,3,[4,5,6],7,[8,9,10,11,[12,1,4,]]]  
+
+const real_another_array = another_arr.flat(Infinity);
+
+console.log(real_another_array);
+
+
+
+
+
+
+console.log(Array.isArray("Ishmeet Singh Sodhi "))
+console.log(Array.from("Ishmeet Singh Sodhi "))
+console.log(Array.from({name: "Hitesh"})) //// instresting case asked in the interview
+
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of (score1,score2,score3));   const heros_marval = ["spiderman ", "ironman ","loki "];
+const heros_dc  = ["batman " ,"Superman " ,"Wonder women "];
+
+
+// heros_dc.push(heros_dc);
+// console.log(heros_marval)
+// console.log(heros_dc);
+// console.log(heros_dc[3][1]);
+
+
+// const allheroes = heros_dc.concat(heros_marval)
+// // console.log(heros_marval);
+// // console.log(heros_dc);
+// console.log(allheroes);
+
+const all_new_heros = [...heros_dc, ...heros_marval];
+// console.log(all_new_heros);
+ 
+
+const another_arr = [ 1,2,3,[4,5,6],7,[8,9,10,11,[12,1,4,]]]  
+
+const real_another_array = another_arr.flat(Infinity);
+
+console.log(real_another_array);
+
+
+
+
+
+
+console.log(Array.isArray("Ishmeet Singh Sodhi "))
+console.log(Array.from("Ishmeet Singh Sodhi "))
+console.log(Array.from({name: "Hitesh"})) //// instresting case asked in the interview
+
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of (score1,score2,score3));   
