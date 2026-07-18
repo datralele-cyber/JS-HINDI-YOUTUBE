f704fe81cebea77bde73e0e3dbfea7e7bbd01014
datralele-cyber <datralele@gmail.com>
2026-07-18 08:12:58 +0000

    just vompleted the objects in the true sence thats it thanks bye

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,6 +1,80 @@
 // Singelton Object Creation 
+// const tinderUser = new Object()       this is singelton 
+const tinderUser = {}        // this is 
+// the above the ways to declare the object in the js thats it thanks 
+ tinderUser.id = "123@abc"
+ tinderUser.name = "Sammy"
+ tinderUser.isloggedIn =  false 
 
 
-const tinderUser = new Object()
 
-console.log
+// console.log(tinderUser);
+
+
+const regularUser = {
+
+    email: "Some@gmail.com",
+
+    fullname :    {
+
+        userFullname: {
+            firstname :  "Ishmeet ",
+            lastName : "Sodhi"
+        }
+
+
+    }
+
+}
+
+// console.log(regularUser.fullname?.userFullname.lastName);       // we can use the question mark if the vaue in the object does not exist 
+
+//  we will work on the. merging of the objects  just like the concatnation of the arrays 
+
+
+
+// const Obj1 = { 1 : "a" , 2: "b"}
+// const Obj2 = { 3 : "c" , 4: "d"}
+// // const Obj3 = {Obj1 , Obj2}
+// // const newObj3 = Object.assign( {} ,Obj1 ,Obj2)
+// const Obj3 = {...Obj1,...Obj2}
+// console.log(Obj3);
+
+
+// from the data base we git it by the following 
+
+
+
+const User = [
+
+    {
+        id : 1 ,
+        email : "IshmeetSinghSOdhi@gmail.com"
+    },
+    {
+        name : "IshmeetSinghSodhi ",
+        location : "Sagar Madhaya Pradesh"
+    },
+    {
+        id : 1 ,
+        email : "IshmeetSinghSOdhi@gmail.com"
+    },
+    {
+        name : "IshmeetSinghSodhi ",
+        location : "Sagar Madhaya Pradesh"
+    },
+    {
+        id : 1 ,
+        email : "IshmeetSinghSOdhi@gmail.com"
+    },
+    {
+        name : "IshmeetSinghSodhi ",
+        location : "Sagar Madhaya Pradesh"
+    },
+]
+User[1].email
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(tinderUser.hasOwnProperty ('isloggedIn'));
+console.log(tinderUser.hasOwnProperty ('islogged'));
