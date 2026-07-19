370edf810dfa6c7a3fc090d95722859a9b20bf7b
datralele-cyber <datralele@gmail.com>
2026-07-19 19:04:58 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,35 @@
+// console.log("I")
+// console.log("S")
+// console.log("H")
+// console.log("M")
+// console.log("E")
+// console.log("E")
+// console.log("T")
+
+
+
+function  sayMyName () {
+    console.log("I")
+console.log("S")
+console.log("H")
+console.log("M")
+console.log("E")
+console.log("E")
+console.log("T")
+
+
+}
+
+// sayMyName() // for execution 
+// sayMyName // for the reference of that the function is in the file thats it 
+
+
+function addTwoNumbers(number1 ,number2 ) {
+    console.log(number1 + number2)
+}
+ addTwoNumbers() // we havnt given it the number in the first stance that we really neeed to but in. (the numbers that we have not entered that is arguments )
+ addTwoNumbers( 3 ,5 )// 8
+ addTwoNumbers( 3 ,"5")// 8
+ addTwoNumbers( 3 ,"a")// 8  
+
+//  fuck offf 
