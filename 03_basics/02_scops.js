c330c61590cdc99e6c8fe291680e19062b613179
datralele-cyber <datralele@gmail.com>
2026-07-20 16:16:54 +0000

    scope block and global scope thats is thanks .

---
+++ 03_basics/02_scops.js
@@ -0,0 +1,29 @@
+// the story start with the let var const 
+
+// let a = 10. // this is the global scope 
+// const b = 20
+// var c = 30
+
+// {} //scope in every programming language 
+
+
+// var c = 300
+
+let a = 200 ;
+
+
+
+
+if(true) {
+
+let a = 10
+const b = 20
+var c = 30 // it is available outside of the scope "IN GLOBAL SCOPE " that is not right so we will not use or avoid using the var in the mordern ontext of the coding in the javascript and the problem still exist if we declare the variable in the scope with the plane c in the context scope space that is the block scope and the values should not go outside of the block sope to the global scope . thanks  
+console.log("INNER : " , a  );
+
+}
+
+ console.log(a);
+ // console.log(b);
+// console.log(c);
+//  the sore scope is differen in the IDE  through "node" and on the browser is different w'll be learning it on the later end .
