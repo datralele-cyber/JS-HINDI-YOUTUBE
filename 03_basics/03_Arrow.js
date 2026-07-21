f61af791ae3e8e1ccbc017b8c53807229cbe0366
datralele-cyber <datralele@gmail.com>
2026-07-21 06:09:22 +0000

    uhjk

---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,12 @@
+const user = {
+    username: "Ishmeet ",
+    price : 999,
+
+    welcomeMessage : function(){
+        console.log(`${this.username} , Welcome to the website `)
+
+    }
+}
+
+
+user.welcomeMessage
