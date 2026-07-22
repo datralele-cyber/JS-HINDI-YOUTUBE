8b59c63e38108099a83b9b3423e1eefaf431a0c0
datralele-cyber <datralele@gmail.com>
2026-07-22 21:26:18 +0000

    just chill

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -25,3 +25,30 @@ const emptyObj  = {}
 if(Object.keys (emptyObj).length === 0) {
  console.log("object is empty ");
 }
+
+
+
+// Nullish Coalescing operator (??) : null and undefined values 
+
+
+let val1;
+
+
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 15  // the first  valid value is been assigned and rest ase been left as the valid first data is been feched 
+
+console.log(val1);
+
+
+//  turnerary operator 
+
+
+// condition ? true : false
+
+
+const iceTeaprice =  100 
+
+
+iceTeaprice  <=  80 ? console.log("less than 80 ") : console.log("more than 80 ")
