159be44779b2ef7f3b6537273f96a76217f734e2
datralele-cyber <datralele@gmail.com>
2026-07-22 19:56:22 +0000

    obj

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,30 +1,73 @@
-// if 
+// // if 
 
 
-if(true) {
-    console.log("If condition is true then the block will get an access thats it ")
-}
-if (false ) { // if the condetion is false then the block will not be getting the access of that scope thanks hitesh sir for the guidence of the right full learing of the practical learning .
-    console.log( " if the given argument is false then we will access this scope ")
+// if(true) {
+//     console.log("If condition is true then the block will get an access thats it ")
+// }
+// if (false ) { // if the condetion is false then the block will not be getting the access of that scope thanks hitesh sir for the guidence of the right full learing of the practical learning .
+//     console.log( " if the given argument is false then we will access this scope ")
     
-}
+// }
 
 
-const is_user_is_logged_in = false 
+// const is_user_is_logged_in = false 
+// const temperature = 41 
 
-if (is_user_is_logged_in) {
+// if (is_user_is_logged_in) {
 
-    console.log("The user is logged in the system thaks for yor concern malik ")
+//     console.log("The user is logged in the system thaks for yor concern malik ")
     
-}
+// }
 
 
 
- // >,<,=>,<= ,==,===,!== 
+//  // >,<,=>,<= ,==,===,!== 
 
 
- if (2 == "2") {
+//  if (2!==3) {
 
-    console.log("executed")
+//     console.log("executed")
     
- }
+//  }
+//  if (temperature < 50)  {  
+//     console.log("temperature less than 50 ")
+    
+//  }
+
+//  console.log("the temerature is above the 50 degrees thanks ")
+
+
+
+// const score = 200
+
+
+
+// if(score >100) {
+//     const power = "fly"
+//     console.log(`user power : ${ power}`)
+// }
+
+
+// console.log(`user power : ${ power}`)
+
+
+//  short hand notation 
+// if  (balance > 500)  console.log("test"),console.log("test 2 ");  this is called as the implicit scope  in the javascript as its executes in the singel line  and multiple line as the comma and that is not an good habit 
+
+
+// nesting 
+
+// const balance = 1000
+// if (balance < 500 ) {
+//     console.log("less than ") ;
+    
+// } else if (balance < 750){
+//     console.log("less than 750");
+    
+
+// } else if (balance <900  ){
+// console.log("less than 750");
+
+// }else{console.log("less than 1200" );
+
+// }
