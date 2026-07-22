153a392011e73705a2428dd56c10f0a0e25cd5d4
datralele-cyber <datralele@gmail.com>
2026-07-22 18:05:12 +0000

    just an up

---
+++ 04_control_flow/one.js
@@ -0,0 +1,30 @@
+// if 
+
+
+if(true) {
+    console.log("If condition is true then the block will get an access thats it ")
+}
+if (false ) { // if the condetion is false then the block will not be getting the access of that scope thanks hitesh sir for the guidence of the right full learing of the practical learning .
+    console.log( " if the given argument is false then we will access this scope ")
+    
+}
+
+
+const is_user_is_logged_in = false 
+
+if (is_user_is_logged_in) {
+
+    console.log("The user is logged in the system thaks for yor concern malik ")
+    
+}
+
+
+
+ // >,<,=>,<= ,==,===,!== 
+
+
+ if (2 == "2") {
+
+    console.log("executed")
+    
+ }
