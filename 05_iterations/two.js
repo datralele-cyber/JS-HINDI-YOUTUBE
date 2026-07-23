20cc86ea34ff6d03d688836aadd5ec4807a510ed
datralele-cyber <datralele@gmail.com>
2026-07-23 12:54:36 +0000

    just completed  while and do while loop in the javascript

---
+++ 05_iterations/two.js
@@ -0,0 +1,27 @@
+// let index = 0 
+// while (index <= 10) {
+//     console.log(`Value of the index is ${index}`);
+//     index = index + 2
+    
+// }
+
+
+// let myArray = [ "flash ","superman","batman ","superwomen " ,"Author " ,"Ishmeet " , "Hitesh "]
+// let arr = 0;
+// while (arr < myArray.length) { 
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr +1 
+    
+// } 
+
+
+
+
+//    =============== do while loop it does what is says thanks 
+
+let score =  11
+do {        
+    console.log(`Score is ${ score }`)
+    score ++ 
+} while (score <= 10 );
+    
