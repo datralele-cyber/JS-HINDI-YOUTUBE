1751520dcc53aae4028017170d9b37fd1ec7c265
datralele-cyber <datralele@gmail.com>
2026-07-24 14:36:50 +0000

    dfd

---
+++ 05_iterations/4four.js
@@ -0,0 +1,33 @@
+const myObject =  {
+    js : "javascipt ",
+    cpp : "C++" ,
+    rb : "ruby " ,
+    swift : "swift by apple "
+
+}
+
+for (const key in myObject) {
+   // console.log(`${key} key is for ${myObject[key]}`);
+    
+}
+
+const programming_arr = ["js","rb","java","cpp"];
+
+for (const key in programming_arr) {
+    // console.log(programming_arr[key]);
+    
+    
+    
+    
+}
+
+// const map = new Map() // map is used  for the display of the uniques values 
+// map.set("IN" , "INDIA")
+// map.set("Fr" , "France")
+// map.set("USA" , "United States of America")
+// // console.log(map);
+// for (const key in map) {
+//     console.key(map[key]) // maps aren't iteratable so there cannot be used in the forin loop thats it thanks 
+    
+    
+// }
---
+++ 05_iterations/5five.js
@@ -0,0 +1,5 @@
+const coding = [ "js","ruby ","python ","java","cpp "]
+coding.forEach( function  (value ) { // function is inside the array so ti will not have an name 
+    console.log(value);
+    
+}) // call back function dont have an name and it is array so it will visit each element in an array 
