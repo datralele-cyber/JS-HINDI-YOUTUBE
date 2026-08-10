fdf699403bfcd509f10d7580e818b4bc9ecde33f
datralele-cyber <datralele@gmail.com>
2026-08-10 17:57:04 +0000

    promises completed til 17: 15 thats it thanks

--- 09_advanse_one/promises.js
+++ 09_advanse_one/promises.js
@@ -8,6 +8,13 @@ const promisOne = new Promise(function(resolve ,reject){
     } ,1000 )
 })
 
+new Promise (function ( resolve , reject ) {
+    setTimeout(function() {
+    console.log("Async task 2 ")    
+    })
+
+})
+
 // promis are created that is resolve and reject now we have to consume that promise 
 promisOne.then(function(){
     console.log("Promise Consumed ");
