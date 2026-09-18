d4774f493b4700e136130d0c0b19d91c98bbb188
datralele-cyber <datralele@gmail.com>
2026-09-18 07:42:42 +0000

    just completed advanse Object Mainuplation and it working

--- 10_class_and_oop/mathPy.js
+++ 10_class_and_oop/mathPy.js
@@ -16,14 +16,29 @@ const descriptor =Object.getOwnPropertyDescriptor(Math , 'PI')
 const chai = {
     username : 'ginger tea ',
     price : '250',
-    isAvailable : true 
+    isAvailable : true ,
+
+    orderChai : function (){ console.log("code fat gaya VALUES NAI AAYI that is Chai nahi bani ")}   // we want the key vaues  
 }
 console.log(Object.getOwnPropertyDescriptor(chai, "username"));
 
 
 Object.defineProperty(chai , "username",{
-  writable: false,
-  enumerable: false,  
+  //writable: false,
+  enumerable: true,  
 } )
 
 console.log(Object.getOwnPropertyDescriptor(chai, "username"));
+
+
+
+for (const  [key ,value ] of Object.entries(chai)) {
+
+    if (typeof  value !== 'function') {
+      console.log(`${key} : ${value }`)  
+    }  
+}
+
+
+
+// TO get an better hold of the above project we really need to build an scalable systems in depth and in much more trapped problen th his scean so we can get hold of the above context and sorry for being verbose that is form side good to see you 
