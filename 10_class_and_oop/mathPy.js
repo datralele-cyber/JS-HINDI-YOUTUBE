aa17b49fae4455380c20bd96ae082562e2382418
datralele-cyber <datralele@gmail.com>
2026-09-18 07:11:28 +0000

    saving my work

---
+++ 10_class_and_oop/mathPy.js
@@ -0,0 +1,24 @@
+const descriptor =Object.getOwnPropertyDescriptor(Math , 'PI')
+
+
+// console.log(descriptor)
+
+// console.log(Math.PI );
+// Math.PI = 5 
+// console.log(Math.PI)
+
+
+
+
+// const mynewObject = Object.create(null) 
+
+
+const chai = {
+    username : 'ginger tea ',
+    price : '250',
+    isAvailable : true 
+}
+console.log(Object.getOwnPropertyDescriptor(chai, "username"));
+
+
+Object
