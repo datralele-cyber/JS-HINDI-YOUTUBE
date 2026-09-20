4daea6157263bbab8b726b3ab82cf6077d25ead6
datralele-cyber <datralele@gmail.com>
2026-09-20 18:52:59 +0000

    just used the getter and setters in the genric form

---
+++ 10_class_and_oop/prototyps_getters_setters.js
@@ -0,0 +1,27 @@
+function User(email, password){
+    this._email = email; // mistake that i havent usedthe underscore in it thanks thta is an bug in it thanks 
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+
+}
+
+const chai = new User("chai@chai.com", "chai")
+
+console.log(chai.email);
+
